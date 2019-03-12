33e66f0ab563fdcf298094ed2da39e73ea7f6c54
AA274 <grantspellman@gmail.com>
2019-03-11 23:03:16 -0700

    grant contributes food_marker_pub v1

--- scripts/food_marker_pub.py
+++ scripts/food_marker_pub.py
@@ -3,33 +3,86 @@
 import rospy
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Quaternion, Pose, Point, Vector3
-from std_msgs.msg import Header, ColorRGBA
+from std_msgs.msg import Header, ColorRGBA, String
 from geometry_msgs.msg import Twist, PoseArray, Pose2D
 from asl_turtlebot.msg import DetectedObject, DetectedObjectList
+import tf
+import numpy as np
 
 class FoodMarkerPublisher():
     def __init__(self):
         rospy.init_node('food_publisher', anonymous=True)
+        
+        rospy.Subscriber('/delivery_request', String, self.food_request_callback)
+                                             
+        self.food_goal_pub = rospy.publisher('/nav_pose', Pose2D, queue_size=10) 
+        
 
-        self.pub= rospy.Publisher('/food_marker', MarkerArray, queue_size=10)
+        # for viz
+        #self.pub = rospy.Publisher('/food_marker', MarkerArray, queue_size=10)
         rospy.Subscriber('/detector/objects', DetectedObjectList, self.food_callback)
+        self.trans_listener = tf.TransformListener()
+        self.food_dict = {} 
 
-        rospy.Subscriber('/detector/objects', DetectedObjectList, self.food_callback)
-        self.object_list = []
+        # Testing 
+        self.fun_stuff = ['banana', 'apple', 'orange', 'stop sign', 'bottle']
+
+        #self.object_list = []
         self.msg_list = []
+
+    def robot_pose(self):
+        try: 
+            origin_frame = "/map" 
+            t, r = self.trans_listener.lookupTransform(origin_frame, "/base_footprint", rospy.Time(0))
+            euler = tf.transformations.euler_from_quaternion(r)
+            return t[0], t[1], euler[2]
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            print("ROBOT POSE FOOD MARKER BAD!!!")
+            pass
+
+    def food_request_callback(self, data):
+        request = data.delivery_request
+        if request in self.food_dict.keys():
+            pose_food = Pose2D()
+            pose_food.x = self.food_dict[request][0]
+            pose_food.y = self.food_dict[request][1]
+            pose_food.theta = self.food_dict[request][2]
+            self.food_goal_pub.publish(pose_food)
+
+    def food_to_world_frame(self, msg):
+        dist = msg.distance
+        robo_x, robo_y, robo_theta = self.robot_pose()
+        food_x = robo_x + dist * np.cos(robo_theta)
+        food_y = robo_y + dist * np.sin(robo_theta)
+        return (food_x, food_y, robo_theta)
+        
 
     def food_callback(self, data):
         ######### YOUR CODE HERE ############
-        # fill out cmd_pose_callback
-        self.object_list = data.objects
-        self.msg_list = data.ob_msgs
+        # make list of whitelisted (aka 'fun' objects) 
+        inds = []
+        for i,obj in enumerate(data.objects):
+            if obj in self.fun_stuff:
+                self.food_dict[obj] = self.food_to_world_frame(data.ob_msgs[i])
+                #self.object_list.append(obj)
+                #inds.append(i)
+
+        #self.msg_list = data.ob_msgs[inds]
 
         ######### END OF YOUR CODE ##########
 
     def run(self):
-    	markerArray = MarkerArray()
+        x, y, theta = self.robot_pose()
+        #markerArray = MarkerArray()
+
         while not rospy.is_shutdown():
+            self.loop()
+            rate.sleep()
+
+            """
         	for msg in self.msg_list:
+                    print(msg)
+                    rospy.loginfo(msg)
 	            circle = Marker()
 	            circle.header.frame_id = "map"
 	            circle.header.stamp = rospy.Time.now()
@@ -61,8 +114,10 @@ class FoodMarkerPublisher():
             #     scale=(5,5,5))
             self.pub.publish(markerArray)
    			rospy.sleep(0.01)
+            """
 
 if __name__ == '__main__':
     m = FoodMarkerPublisher()
+    rospy.spin()
     print "running target marker"
-    m.run()
+    #m.run()
